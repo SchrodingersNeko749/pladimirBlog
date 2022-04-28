9560edb5eeb5566ab288fb65b10a0e650b622d77
NekoSina <nekosalad@protonmail.com>
2022-04-28 17:06:26 +0430

    onFileChangeHook

--- updateblog.sh
+++ updateblog.sh
@@ -2,5 +2,5 @@
 
 JEKYLL_ENV=production bundle exec jekyll build
 git add . && git commit -m 'onFileChangeHook' && git push
-rsync -aAX0PH -e "ssh -o Compression=no" _site ubuntu@pladimirvutin.her.st:/home/ubuntu/Server
+rsync -aAX0PH -e "ssh -o Compression=no" _site ubuntu@pladimirvutin.her.st:/srv/www/blog
 echo "done!"
