e8754246ed1796e0951717d12d42b9f6aa40101a
NekoSina <nekosalad@protonmail.com>
2022-11-29 12:56:15 +0330

    onFileChangeHook

--- updateblog.sh
+++ updateblog.sh
@@ -3,4 +3,5 @@
 JEKYLL_ENV=production bundle exec jekyll build
 git add . && git commit -m 'onFileChangeHook' && git push
 rsync -aAX0PH -e "ssh -p 10422 -o Compression=no" _site root@eu1.her.st:/srv/neko.her.st
+scp -r _site neko@box.alumni.re:/neko.her.st
 echo "done!"
